98b6158b221223c7fab61b9796f9dc9c5f2a8b7d
fahmimubarok03 <m.mubarok23@student.uisi.ac.id>
2025-11-24 22:14:38 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
-import 'pages/register_page.dart';
+import 'pages/login_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
+      title: "E-Perpus",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey.shade100,
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.blue.shade700,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            minimumSize: const Size(double.infinity, 45),
+            padding: const EdgeInsets.all(12),
+          ),
+        ),
+      ),
+      home: const LoginPage(),
     );
   }
 }
